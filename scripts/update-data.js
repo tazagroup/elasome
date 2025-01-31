994c10419363b80aa94239f329f48f73df33ec6b
quocbao1983 <quocbao280783@gmail.com>
2025-02-01 01:32:06 +0700

    update

--- scripts/update-data.js
+++ scripts/update-data.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
-
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTRFQiOiIwOTc3MjcyOTY3IiwiZW1haWwiOiJjaGlraWV0ODhAZ21haWwuY29tIiwiZ2lkIjoiMTAzMzg2Mjc5NjM1MjIxMDg4MTM0IiwiaWF0IjoxNzM4MzQ4MDI1LCJleHAiOjE3NDA5NDAwMjV9.zK2TuPxIJ9Cvn9YWICpVsRKf6Ra4nKyokVyhpFU69pc";
 async function updateData() {
   try {
     // Fetch dynamic data from your API
-    const response = await fetch('https://api.example.com/data'); // Replace with your API endpoint
-
+    const response = await fetch('https://api.tazagroup.vn/users', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    });
     // Check if the response is OK
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -14,7 +19,7 @@ async function updateData() {
     const data = await response.json();
 
     // Save the data to a file in the Angular assets folder
-    fs.writeFileSync('src/assets/data.json', JSON.stringify(data));
+    fs.writeFileSync('public/assets/data.json', JSON.stringify(data));
     console.log('Dynamic data updated successfully!');
   } catch (error) {
     console.error('Error updating dynamic data:', error);
