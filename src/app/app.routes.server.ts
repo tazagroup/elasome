a62db30c7981089758880318c24e16c036a7ac8a
quocbao1983 <quocbao280783@gmail.com>
2025-02-01 02:59:12 +0700

    update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,17 +5,27 @@ import { UsersService } from './admin/adminmain/listuser/listuser.services';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: 'admin/users/:id',
     renderMode: RenderMode.Server,
-    async getPrerenderParams() {
-      const _UsersService = inject(UsersService);
-      const ids = await _UsersService.getUsers(); // Assuming this returns ['1', '2', '3']
-      return ids.map((id:any) => ({ id })); // Transforms IDs into an array of objects for prerendering
-      // This will prerender the paths: `/post/1`, `/post/2` and `/post/3`
-    },
   },
+  {
+    path: 'admin/danhmuc/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'admin/baiviet/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'admin/sanpham/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: ':slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
