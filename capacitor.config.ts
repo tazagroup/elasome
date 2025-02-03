a93183ab3aeb5a4f838b2d224a2e5e52438e2545
quocbao1983 <quocbao280783@gmail.com>
2025-02-03 12:29:28 +0700

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobielasome',
+  webDir: 'www'
+};
+
+export default config;
