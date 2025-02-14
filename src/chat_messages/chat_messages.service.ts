a8b14c586ae74ec8594027bd0c35ae08a60cee1f
chikiet88 <chikiet88@gmail.com>
2025-02-14 12:06:24 +0700

    update

--- src/chat_messages/chat_messages.service.ts
+++ src/chat_messages/chat_messages.service.ts
@@ -29,14 +29,14 @@ import { Injectable } from '@nestjs/common';
     async findSHD(data: any) {
       return await this.Chat_messagesRepository.findOne({
         where: {
-          Title: data.Title,
+
           Type: data.Type
         },
       });
     }
     async findslug(Title: any) {
       return await this.Chat_messagesRepository.findOne({
-        where: { Title: Title },
+  
       });
     }
     async findPagination(page: number, perPage: number) {
--- src/chat_participants/chat_participants.service.ts
+++ src/chat_participants/chat_participants.service.ts
@@ -29,14 +29,14 @@ import { Injectable } from '@nestjs/common';
     async findSHD(data: any) {
       return await this.Chat_participantsRepository.findOne({
         where: {
-          Title: data.Title,
+
           Type: data.Type
         },
       });
     }
     async findslug(Title: any) {
       return await this.Chat_participantsRepository.findOne({
-        where: { Title: Title },
+
       });
     }
     async findPagination(page: number, perPage: number) {
